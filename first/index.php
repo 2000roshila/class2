90368bdfeabc5ca6f2f521cd9817d4e428b6bbf2
Roshila Hatharasinghe <85453926+2000roshila@users.noreply.github.com>
2022-06-24 20:10:45 +0530

    Add files via upload

---
+++ first/index.php
@@ -0,0 +1,25 @@
+<title>Hatharasinghe Roshila PHP</title>
+<h1>Hatharasinghe Roshila PHP</h1>
+<p>
+<?php
+echo('The SHA256 hash of "Hatharasinghe Roshila" is ');
+print hash('sha256','Hatharasinghe Roshila');
+?>
+</p>
+<p>ASCII ART:</p>
+
+<pre>
+    **     **
+    **     **
+    **     ** 
+    *********
+    **     **
+    **     **
+    **     **
+</pre>
+</p>
+
+<a href="http://localhost/first/check.php">Click here to check the error setting</a>
+<br>
+<a href="http://localhost/first/fail.php">Click here to cause a traceback</a>
+
